968b21cdc10c73aec1e77a1edbc5663ea69cb59b
Hamza Mehyar <hamza.r.mehyar@gmail.com>
2022-11-28 12:59:57 +0300

    ok

--- .config/i3/volume.sh
+++ .config/i3/volume.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
 default_sink=$( pacmd stat | grep "Default sink" | awk '{print substr($4,0) " "}' )
 
-pactl set-sink-volume $default_sink $1
+current_volume_pc=$(pactl list sinks | grep '^[[:space:]]Volume:' | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+
+  echo "+$2%"
+if [ $1 == "+" ] && [ $current_volume_pc -lt  100 ]; then
+  pactl set-sink-volume $default_sink "+$2%"
+fi
+
+if [ $1 == "s" ]; then
+  pactl set-sink-volume $default_sink "$2%"
+fi
+
+if [ $1 == "-" ]; then
+  pactl set-sink-volume $default_sink "-$2%"
+fi
